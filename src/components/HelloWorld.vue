c2930a9fcb0896fabb0c2838db246c9c5d63de67
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-08-01 09:47:33 +0900

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <h2>Essential Links</h2>
+    <button @click="callPublic">public</button>
+    <button @click="callPrivate">private</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const ROOT_URL = 'http://localhost:8000'
+const PUBLIC_API_URL = `${ROOT_URL}/public`
+const PRIVATE_API_URL = `${ROOT_URL}/private`
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    async callPublic () {
+      const res = await axios.get(PUBLIC_API_URL)
+      this.message = res.data
+    },
+    async callPrivate () {
+      const res = await axios.get(PRIVATE_API_URL)
+      this.message = res.data
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
---
+++ src/components/Signin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="signin">
+    <h2>Sign in</h2>
+    <input type="text" placeholder="username" v-model="email">
+    <input type="password" placeholder="password" v-model="password">
+    <button>Signin</button>
+    <p>If you don't have an account
+      <router-link to="/signup">create your account now</router-link>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.signin {
+  margin-top: 20px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  margin: 10px 0;
+  padding: 10px;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
